71bb34f2db93c1e322c5d6b65593bd9ece5ded6c
ynswtf <yunussenem@icloud.com>
2020-10-09 17:11:34 +0300

    Yandex

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 return [
-    'name' => 'googleanalytics'
+    'name' => 'yandex',
+
+    'config' => [
+        'id' => '026d8ce163714fd9962eaa7ebfff6f40',
+        'password' => '3d65cdaf4ff4443c87fde38247d1db22'
+    ]
 ];
